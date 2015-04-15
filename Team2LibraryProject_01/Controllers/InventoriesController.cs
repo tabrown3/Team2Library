fd1c1b696249214bf4614310c20bb4fde35f2276
Jay Brandin <vktrstorm@users.noreply.github.com>
2015-04-14 20:42:24 -0500

    added inventory page and add book to the admin page

---
+++ Team2LibraryProject_01/Controllers/InventoriesController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Team2LibraryProject_01.Models;
+
+namespace Team2LibraryProject_01.Controllers
+{
+    public class InventoriesController : Controller
+    {
+        private Team2LibraryEntities db = new Team2LibraryEntities();
+
+       
+        // GET: Inventories
+        public ActionResult Index()
+        {
+            if (User.IsInRole("Admin"))
+            {
+                var inventories = db.Inventories.Include(i => i.Book);
+                return View(inventories.ToList());
+            }
+            else
+                return View("~/Views/Home/Index.cshtml");
+           
+        }
+
+        // GET: Inventories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Inventories.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // GET: Inventories/Create
+        public ActionResult Create()
+        {
+            ViewBag.ISBN = new SelectList(db.Books, "ISBN", "Title");
+            return View();
+        }
+
+        // POST: Inventories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemID,ItemPrice,DateAdded,ISBN")] Inventory inventory)
+        {
+            //Insert same values to Member relation
+            Random rand = new Random();
+            int inventoryID = rand.Next(0, 5000);
+
+            if (ModelState.IsValid)
+            {
+                inventory.ItemID = inventoryID;
+                db.Inventories.Add(inventory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ISBN = new SelectList(db.Books, "ISBN", "Title", inventory.ISBN);
+            return View(inventory);
+        }
+
+        // GET: Inventories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Inventories.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ISBN = new SelectList(db.Books, "ISBN", "Title", inventory.ISBN);
+            return View(inventory);
+        }
+
+        // POST: Inventories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemID,ItemPrice,DateAdded,ISBN")] Inventory inventory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(inventory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ISBN = new SelectList(db.Books, "ISBN", "Title", inventory.ISBN);
+            return View(inventory);
+        }
+
+        // GET: Inventories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Inventories.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Inventory inventory = db.Inventories.Find(id);
+            db.Inventories.Remove(inventory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
